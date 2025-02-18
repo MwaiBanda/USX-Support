1530197248990acc8028bc432e255eda69b1b4ac
Mwai Banda <bandamwai@gmail.com>
2025-02-18 15:32:36 -0600

    added react-router for dynamic routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { useQuery } from '@apollo/client';
 import './App.css'
 import { GET_CHAPTERS } from './graphql/queries.graphql';
 import { BibleBookQuery, BibleBookQueryVariables } from './graphql/__generated__/graphql';
+import { useParams } from 'react-router';
 
 function App() {
+  let params = useParams();
+
   const { loading, error, data } = useQuery<BibleBookQuery, BibleBookQueryVariables>(
     GET_CHAPTERS,
-    { variables: { id: 'NLT.JHN' }}
+    { variables: { id: `${params.version?.toUpperCase()}.${params.book?.toUpperCase()}` }}
   );
 
   if (loading) return <p>Loading...</p>;
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,20 @@ import './index.css'
 import App from './App.tsx'
 import { ApolloProvider } from '@apollo/client'
 import { client } from './lib/apollo.ts'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <ApolloProvider client={client}>
-        <App />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/:version/:book" element={<App />} />
+            <Route
+              path="*"
+              element={<Navigate to="/nlt/jhn" />}
+            />         
+          </Routes>
+        </BrowserRouter>
       </ApolloProvider>
   </StrictMode>,
 )
